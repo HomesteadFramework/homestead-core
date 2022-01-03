8b1c4a4e6ac72b0cc94f51b84b526267592189e6
Brandon Braner <brandon.braner@gmail.com>
2022-01-02 21:08:06 -0600

    automatic route loading for modules

--- homestead/__init__.py
+++ homestead/__init__.py
@@ -1,4 +1,5 @@
 from .core.main import Homestead
-from .core.routing import WebRouter as WebRouter
+from .core.routes.routing import WebRouter
+from fastapi import APIRouter as APIRouter
 
 __version__ = '0.1.0'
