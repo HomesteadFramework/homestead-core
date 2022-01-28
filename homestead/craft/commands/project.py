701a1a1e2a12eec82190a513a085b9e22a9d14dd
Brandon Braner <brandon.braner@gmail.com>
2022-01-27 21:33:47 -0600

    wip

---
+++ homestead/craft/commands/project.py
@@ -0,0 +1,21 @@
+from cookiecutter.main import cookiecutter
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def init():
+    project_name = typer.prompt("Enter your project name: ")
+    project_dir = typer.prompt("Where should your project be created: ")
+
+    project_dir = project_dir.rstrip('/')
+    project_path = f"{project_dir}/{project_name}"
+    cookiecutter(
+        'https://github.com/HomesteadFramework/cookiecutter.git',
+        output_dir=project_path
+    )
+
+
+if __name__ == "__main__":
+    app()
