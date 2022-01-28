af28f2a381d059a4590040aca4e413f9299a251b
Brandon Braner <brandon.braner@gmail.com>
2022-01-27 22:01:22 -0600

    wip

--- homestead/craft/commands/project.py
+++ homestead/craft/commands/project.py
@@ -6,18 +6,12 @@ app = typer.Typer()
 
 @app.command()
 def init():
-    project_name = typer.prompt("Enter your project name: ")
-    project_dir = typer.prompt("Where should your project be created: ")
-
+    project_dir = typer.prompt("Where should your project be created: Please enter the full path")
     project_dir = project_dir.rstrip('/')
-    project_path = f"{project_dir}/{project_name}"
-    extra_context = {
-        "app_name": project_name,
-    }
+
     cookiecutter(
         'https://github.com/HomesteadFramework/cookiecutter.git',
-        output_dir=project_path,
-        extra_context=extra_context,
+        output_dir=project_dir,
     )
 
 
